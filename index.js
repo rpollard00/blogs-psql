56626e97892547a9bb63e97eb286c87d039d2300
Reese Pollard <rpollard@gmail.com>
2022-10-10 14:38:38 -0700

    13.4 basic REST backend commands

--- index.js
+++ index.js
@@ -5,21 +5,90 @@ const app = express()
 app.use(express.json())
 
 const sequelize = new Sequelize(process.env.DATABASE_URL)
-
-const main = async () => {
-  try {
-    await sequelize.authenticate()
-    const blogs = await sequelize.query("SELECT * FROM blogs", {
-      type: QueryTypes.SELECT,
-    })
-    blogs.map(blog =>
-      console.log(`${blog.author}: '${blog.title}' ${blog.likes} likes`)
-    )
-    console.log(blogs)
-    sequelize.close()
-  } catch (error) {
-    console.error("Unable to connect to the database:", error)
+// model of Blog
+class Blog extends Model {}
+Model.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    author: {
+      type: DataTypes.TEXT,
+    },
+    url: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: "blog",
   }
-}
+)
 
-main()
+// get api/blogs
+app.get("/api/blogs", async (req, res) => {
+  // findAll
+  const blogs = await Blog.findAll()
+  console.log(JSON.stringify(blogs, null, 2))
+  // response
+  res.json(blogs)
+})
+
+// post api/blogs
+app.post("/api/blogs", async (req, res) => {
+  try {
+    console.log(req.body)
+    const blog = await Blog.create(req.body)
+    res.json(blog)
+  } catch (error) {
+    return res.status(400).json({ error })
+  }
+})
+
+// delete api/blogs/:id
+app.delete("/api/blogs/:id", async (req, res) => {
+  try {
+    const blogToDelete = await Blog.findByPk(req.params.id)
+    await blogToDelete.destroy()
+    res.status(200).send()
+  } catch (error) {
+    return res.status(400).json({ error })
+  }
+})
+
+// server listen on 3001
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
+// const main = async () => {
+//   try {
+//     await sequelize.authenticate()
+//     const blogs = await sequelize.query("SELECT * FROM blogs", {
+//       type: QueryTypes.SELECT,
+//     })
+//     blogs.map(blog =>
+//       console.log(`${blog.author}: '${blog.title}' ${blog.likes} likes`)
+//     )
+//     console.log(blogs)
+//     sequelize.close()
+//   } catch (error) {
+//     console.error("Unable to connect to the database:", error)
+//   }
+// }
+
+// main()
