915a810dba99bad553e1790588f2b58eb367c0b0
Reese Pollard <rpollard@gmail.com>
2022-10-10 10:42:34 -0700

    13.3 query blogs with SQL

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require("dotenv").config()
-const { Sequelize, Model, DataTypes } = require("sequelize")
+const { Sequelize, QueryTypes, Model, DataTypes } = require("sequelize")
 const express = require("express")
 const app = express()
 app.use(express.json())
@@ -9,7 +9,13 @@ const sequelize = new Sequelize(process.env.DATABASE_URL)
 const main = async () => {
   try {
     await sequelize.authenticate()
-    console.log("Connected successfully initiated.")
+    const blogs = await sequelize.query("SELECT * FROM blogs", {
+      type: QueryTypes.SELECT,
+    })
+    blogs.map(blog =>
+      console.log(`${blog.author}: '${blog.title}' ${blog.likes} likes`)
+    )
+    console.log(blogs)
     sequelize.close()
   } catch (error) {
     console.error("Unable to connect to the database:", error)
