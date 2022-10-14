498a66db4dc5be7d9d1f3f5d707fd0b79f762d7c
Reese Pollard <rpollard@gmail.com>
2022-10-14 09:27:39 -0700

    13.9 require that username is a valid email address

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -3,8 +3,17 @@ const { response } = require("express")
 const errorHandler = (error, req, res, next) => {
   console.error(error.message)
   console.error("ERROR NAME:", error.name)
+  console.error("ERROR FULL", error)
 
   if (error.name === "SequelizeValidationError") {
+    const errorTypeName = error.errors[0].validatorName
+
+    if (errorTypeName === "isEmail") {
+      return res.status(400).send({
+        error: "Unable to create user. Username must be a valid email address.",
+      })
+    }
+
     return res.status(400).send({
       error:
         "Unable to validate request body. Check for missing non-nullable fields.",
